dc67853bcf1e7d006cd8aa4fe2d1f2d042a8c7a4
Yuniel Acosta Pérez <yuniel.acosta@cav.desoft.cu>
2021-07-06 19:57:26 -0400

    Vite icon issue solved. Added iconify icon package.

--- src/components/ProjectTechList.vue
+++ src/components/ProjectTechList.vue
@@ -3,10 +3,12 @@
     <li v-for="tech in project.tech" :key="tech.id"
         class="my-2 text-light-slate font-mono text-xs whitespace-nowrap" :class="classLinks">
       <g-link v-if="tech.url" class="flex" :to="tech.url">
-        <font-awesome v-if="tech.icon && showIcon && tech.icon.type !== 'simple-icon'"
+        <font-awesome v-if="tech.icon && showIcon && tech.icon.type === 'font-awesome'"
                       :icon="(typeof tech.icon === 'string')?['fab', tech.icon]:[tech.icon.type, tech.icon.value ]"/>
         <simple-icon-wrapper v-if="tech.icon && showIcon && tech.icon.type === 'simple-icon'"
                              :icon-component="tech.icon.value"/>
+        <iconify-wrapper v-if="tech.icon && showIcon && tech.icon.type === 'iconify'"
+                         :icon="tech.icon.value"/>
         <span class="mx-1">{{ tech.name }}</span>
       </g-link>
       <span v-else>{{ tech.name }}</span>
@@ -19,9 +21,10 @@ import 'reflect-metadata';
 import {Component, Prop, Vue} from "vue-property-decorator";
 import Project from '~/models/Project';
 
-const SimpleIconWrapper = () => import("~/components/shared/SimpleIconWrapper.vue");
+const SimpleIconWrapper = () => import("~/components/icons/SimpleIconWrapper.vue");
+const IconifyWrapper = () => import("~/components/icons/IconifyWrapper.vue");
 
-@Component({components: {SimpleIconWrapper}})
+@Component({components: {SimpleIconWrapper, IconifyWrapper}})
 export default class ProjectTechList extends Vue {
   @Prop({default: 'end', type: String}) readonly position: string | undefined;
   @Prop({default: true, type: Boolean}) readonly showIcon: boolean | undefined;
